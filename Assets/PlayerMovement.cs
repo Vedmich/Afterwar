9b6392c627d09521eb66b43aee01a84b6cecb847
Nick Filatov <nick.filatov@cloudcastlegroup.com>
2016-05-18 18:44:54 +0400

    some old stuff

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+
+	private Animator playerAnimator;
+
+	void Awake() 
+	{
+		playerAnimator = GetComponent<Animator>();
+	}
+
+	void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+	}
+
+	void MovementManagement(float horizontal, float vertical)
+	{
+		if(horizontal !=0f || vertical !=0f)
+		{
+		}
+	}
+
+	void Rotating(float horizontal, float vertical)
+	{
+		Vector2 targetDirection = new Vector2(horizontal, vertical);
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector2.up);
+		/*Quaternion newRotation = Quaternion.Lerp(rigidbody2D.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		rigidbody.MoveRotation(newRotation);*/
+	}
+}
--- Assets/Scripts/NPCs/CitizensList.cs
+++ Assets/Scripts/NPCs/CitizensList.cs
@@ -5,51 +5,16 @@ using System.Collections.Generic;
 public class CitizensList : MonoBehaviour 
 {
 
-	public List<CitizensData> citizens;// = new List<Citizen>();
+	public List<CitizensData> citizens;
+	public Citizen CitizenPrefab;
 
-	int index = Random.Range(0, List<citizens>.Count);
 		
 	void Start () 
 	{
+		int index = Random.Range(0, citizens.Count);
 
-		// Citizen (name, stressOnDeath, woundsInflicted)
-
-		/*citizens.Add( new Citizen ("Harvey", 0, 1));
-		citizens.Add( new Citizen ("Joker", 0, 3));
-		citizens.Add( new Citizen ("Harley", 0, 2));
-		citizens.Add( new Citizen ("A", 0, 2));
-		citizens.Add( new Citizen ("B", 0, 2));
-		citizens.Add( new Citizen ("C", 0, 2));
-		citizens.Add( new Citizen ("D", 0, 2));
-		citizens.Add( new Citizen ("E", 0, 2));
-		citizens.Add( new Citizen ("F", 0, 2));
-		citizens.Add( new Citizen ("G", 0, 2));
-
-		citizens.Add( new Citizen ("Aeris", 2, 0));
-		citizens.Add( new Citizen ("1", 2, 0));
-		citizens.Add( new Citizen ("2", 2, 0));
-		citizens.Add( new Citizen ("3", 2, 0));
-		citizens.Add( new Citizen ("4", 2, 0));
-		citizens.Add( new Citizen ("5", 2, 0));
-		citizens.Add( new Citizen ("6", 2, 0));
-		citizens.Add( new Citizen ("7", 2, 0));
-		citizens.Add( new Citizen ("8", 2, 0));
-		citizens.Add( new Citizen ("9", 2, 0));
-		citizens.Add( new Citizen ("10", 2, 0));
-		citizens.Add( new Citizen ("11", 2, 0));
-		citizens.Add( new Citizen ("12", 2, 0));
-		citizens.Add( new Citizen ("13", 2, 0));
-		citizens.Add( new Citizen ("14", 2, 0));
-		citizens.Add( new Citizen ("15", 2, 0));
-		citizens.Add( new Citizen ("16", 2, 0));
-		citizens.Add( new Citizen ("17", 2, 0));
-		citizens.Add( new Citizen ("18", 2, 0));
-		citizens.Add( new Citizen ("19", 2, 0));
-		citizens.Add( new Citizen ("20", 2, 0));
-		citizens.Add( new Citizen ("21", 2, 0));
-		citizens.Add( new Citizen ("22", 2, 0));
-		citizens.Add( new Citizen ("23", 2, 0));
-		citizens.Add( new Citizen ("24", 2, 0));*/
+		Citizen newCit = Instantiate(CitizenPrefab);
+		newCit.Init(citizens[index].name,3,3);
 
 	}
 
